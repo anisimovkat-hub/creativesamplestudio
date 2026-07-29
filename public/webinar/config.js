0aa0b99cf7172607ed89fc5fb5dc5355a03834b5
anisimovkat-hub <anisimov.kat@gmail.com>
2026-07-29 12:32:05 +0300

    Update public/webinar/config.js for webinar pre-registration

--- public/webinar/config.js
+++ public/webinar/config.js
@@ -1,9 +1,6 @@
 window.CSS_WEBINAR_CONFIG = Object.freeze({
-  webinarId: "CSS-WEB-2026-07-31",
-  displayDate: "31 July 2026 · 12:00 PM New York / 5:00 PM London",
+  webinarId: "CSS-WEB-FASHION-BRAND-AS-A-SYSTEM",
+  displayDate: "Date to be announced",
   registrationEndpoint: "/api/register",
-  recordingPrice: 30,
-  recordingCurrency: "USD",
-  recordingCheckoutUrl: "",
   metaPixelId: "2128389784386493"
 });
